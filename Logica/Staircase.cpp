0be508fb37a00b3bd74d6765073e77c852caf507
Lucero Obispo <91815464+luceroobispo@users.noreply.github.com>
2022-05-26 22:21:03 -0500

    Create Staircase.cpp

---
+++ Logica/Staircase.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++) {
+		for (int k = n - i - 1; k > 0; k--)
+			cout << " ";
+		for (int j = 0; j <= i; j++)
+			cout << "#";
+		cout << endl;
+	}
+    return 0;
+}
